b2609b23580c804b0dceda7ae60a284c46aa3be1
Sagacode <ishimwe.sage.3@gmail.com>
2024-07-25 00:36:24 +0200

    chore: Refactor page.tsx and AddTodoForm.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
   return (
-    <main>
-      <Button>Click me</Button>
+    <main className="flex flex-col justify-center h-screen bg-sky-100">
+      {/* welcome to super todo app */}
+      <h1 className="text-4xl font-bold text-center text-sky-900">
+        Welcome to Super Todo App
+      </h1>
+      <p className="text-center text-sky-700">
+        Get started by creating a new todo
+      </p>
+      <div className="flex justify-center mt-4">
+        <Button onClick={() => router.push("/todo")}>Get Start</Button>
+      </div>
     </main>
   );
 }
