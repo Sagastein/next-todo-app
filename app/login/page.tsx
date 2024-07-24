6727e491ba75a8ac5b9254f41603e0e336d9c7c0
Ishimwe Sage <72102004+Sagastein@users.noreply.github.com>
2024-07-25 01:02:36 +0200

    Ft user layout UI (#1)
    * setup empty next app with shadcn
    
    * add drizzle orm in the project
    
    * chore: Add login page with TodoForm component

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import { TodoForm } from "@/components/ui/AddTodoForm";
+
+function page() {
+  return (
+    <div>
+      welcome to login page
+      <TodoForm />
+    </div>
+  );
+}
+
+export default page;
